67a6a201629e0969bf5e1631038f71e4debc5717
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-03-06 02:48:29 +0300

    New Project Added

---
+++ Csharp_Essential2/BasicInCsharp/Methods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInCsharp
+{
+    public class Methods
+    {
+        #region Factorial Method
+
+        static int Factorial(int x)
+        {
+            if (x <= 0)
+            {
+                return 1;
+            }
+
+            return x * Factorial(x-1);
+        }
+
+        #endregion
+
+        #region Methods
+
+        // No parameters - No Return Value
+        static void WriteScreen()
+        {
+            Console.WriteLine("Hello, I learn C#");
+        }
+
+        // Parameter Field - Not Returning Value
+        static void WriteScreen(string Name)
+        {
+            Console.WriteLine("$Hello {Name}");
+        }
+
+        // Parameter Field - Returning Value
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        // params keyword -> use this keyword when the number of parameters not clear
+        static int SumParams(params int[] values)
+        {
+            int Addition = 0;
+
+            foreach (var i in values)
+            {
+                Addition += i;
+            }
+
+            return Addition;
+        }
+
+        // Takes an array as parameter - Returns array
+        static int[] Square(int[] Array)
+        {
+            // make an object
+            int[] arr = new int[Array.Length];
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                arr[i] = Array[i] * Array[i];
+            }
+
+            return arr;
+        }
+
+        #endregion
+
+        #region Lambda Functions
+        static Func<int,int,int> Sum2 = (i, j) => i + j;
+
+        static Func<int,int> Square2 = x => x * x;
+
+        static Action<string> Warning = Name => Console.WriteLine("${Name} Warning");
+        #endregion
+    }
+}
---
+++ Csharp_Essential2/BasicInCsharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Explanation About Code
+/*
+ Purpose of the code is learn some essentials about C#
+
+Developer: Barış Someroğlu
+Date: 06.03.2025 - 01:10 am
+*/
+#endregion
+
+namespace BasicInCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the background and text color
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
