87bf98e196dddd3fcb1f2912b836bdaa189ca480
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-03-05 20:29:09 +0300

    Codes Updated

--- Csharp_Essential1/ConsoleApp1/Program.cs
+++ Csharp_Essential1/ConsoleApp1/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +9,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello C#");
         }
     }
 }
