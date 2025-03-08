25be83a1c34dbb15177b8d052031b9633e6c3090
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-03-09 00:54:45 +0300

    Wrote a Simple Code to Understand Properties

---
+++ PropertyCodes/ConsoleApp1/ControlAndCalculate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ControlAndCalculate
+    {
+        private double _examscore1 = 0.0d;
+        private double _examscore2 = 0.0d;
+        public double TotalExamScore = 0.0d;
+
+        public double _ExamScore1
+        {
+            get
+            {
+                if (_examscore1 < 0)
+                {
+                    return 0;
+                }
+
+                else
+                {
+                    return _examscore1;
+                }
+            }
+
+            set
+            {
+                _examscore1 = value;
+
+                TotalExamScore += _examscore1;
+            }
+        }
+
+        public double _ExamScore2
+        {
+            get
+            {
+                if (_examscore2 < 0)
+                {
+                    return 0;
+                }
+
+                else
+                {
+                    return _examscore2;
+                }
+            }
+
+            set
+            {
+                _examscore2 = value;
+
+                TotalExamScore += _examscore2;
+            }
+        }
+
+        public void AvverageExam()
+        {
+            Console.WriteLine($"\nAvverage of Exam: {TotalExamScore / 2}");
+        }
+    }
+}
---
+++ PropertyCodes/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // define variables
+            double ExamScore1 = 0.0f;
+            double ExamScore2 = 0.0f;
+
+            // get value from user for variables
+            Console.Write("Please Enter 1. Exam Score: ");
+            ExamScore1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nPlease Enter 2. Exam Score: ");
+            ExamScore2 = Convert.ToDouble(Console.ReadLine());
+
+            ControlAndCalculate o1 = new ControlAndCalculate();
+
+            o1._ExamScore1 = ExamScore1;
+            o1._ExamScore2 = ExamScore2;
+
+            o1.AvverageExam();
+        }
+    }
+}
