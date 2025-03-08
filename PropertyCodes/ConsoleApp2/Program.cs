0e9cb7c8f5b5e7f946e5d43cb613af6dceef1b7b
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-03-09 01:11:12 +0300

    Added New Codes

---
+++ PropertyCodes/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // define variables
+            double Salary = 0.0D;
+
+            // get value from user
+            Console.Write("Please Enter Your Salary: ");
+            Salary = Convert.ToDouble(Console.ReadLine());
+
+            Worker w1 = new Worker();
+
+            w1._Salary = Salary;
+
+            Console.WriteLine($"Salary is {w1._Salary}");
+        }
+    }
+}
---
+++ PropertyCodes/ConsoleApp2/Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Worker
+    {
+        private double _salary = 0.0d;
+        private double minSalary = 5000;
+        public double _Salary
+        {
+            get { return _salary; }
+
+            set
+            {
+                if (value >= minSalary)
+                {
+                    _salary = value;
+                }
+
+                else
+                {
+                    Console.WriteLine($"Wrong Amount! Minimum Salary is {minSalary}");
+                }
+            }
+        }
+    }
+}
