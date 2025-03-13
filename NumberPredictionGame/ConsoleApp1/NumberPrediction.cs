9183fb9a339902e1a538b1f52d17f70a8870d2fd
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-03-14 00:37:54 +0300

    Added New Project

---
+++ NumberPredictionGame/ConsoleApp1/NumberPrediction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NumberPrediction
+{
+    public void ControlMethod(int RandomNumber)
+    {
+        int PredictionCounter = 0;
+        int UserPrediction = -1;
+
+        while (UserPrediction != RandomNumber)
+        {
+            // Get value from user
+            Console.Write("Please Enter Your Prediction: ");
+            UserPrediction = Convert.ToInt32(Console.ReadLine());
+            PredictionCounter++;
+
+            if (UserPrediction < RandomNumber)
+            {
+                Console.WriteLine("Input a bigger number!");
+            }
+            else if (UserPrediction > RandomNumber)
+            {
+                Console.WriteLine("Input a less number!");
+            }
+        }
+
+        Console.WriteLine("Congrats you found the number!");
+        Console.WriteLine($"You found the number at {PredictionCounter}. try");
+
+        Console.WriteLine("End of the game!");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.Write($"Game is closing %{i} \r"); // \r return the first line
+            System.Threading.Thread.Sleep(30); // wait 30 ms
+        }
+
+        Console.WriteLine("\nGame closed!");
+    }
+}
---
+++ NumberPredictionGame/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Developer: Barış Someroğlu
+/*
+Purpose of the code is make a number prediction game
+
+Date: 14.03.2025 - 00:30 am
+*/
+#endregion
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Random RandomNumberObject = new Random();
+            int RandomNumber = RandomNumberObject.Next(1, 101);
+
+            Console.WriteLine("\t\t\t\t\t WELCOME TO THE NUMBER PREDICTION GAME!");
+            Console.WriteLine("\nRules of the game:");
+            Console.WriteLine("1. Computer generates a random number between 1 and 100");
+            Console.WriteLine("2. According to your input, you will see a feedback message");
+            Console.WriteLine("\n\t\t\t Let's start and see after how many prediction you will find the correct number");
+
+            NumberPrediction o1 = new NumberPrediction();
+            o1.ControlMethod(RandomNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
