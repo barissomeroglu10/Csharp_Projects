d93861c573266a67f463876086b2c98c8eca0770
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-03-18 06:00:13 +0300

    Added New Project

---
+++ CarGallery/ConsoleApp1/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        // Fields
+        private string _carname;
+        private string _carmodel;
+        private int _carprice;
+
+        // Properties
+        public string Carname
+        {
+            get { return _carname; }
+            set { _carname = value; }
+        }
+
+        public string Carmodel
+        {
+            get { return _carmodel; }
+            set { _carmodel = value; }
+        }
+
+        public int Carprice
+        {
+            get { return _carprice; }
+            set { _carprice = value; }
+        }
+
+        public void Cars(string _carname)
+        {
+            if (_carname == "Audi")
+            {
+                AudiCars();
+            }
+        }
+
+        public void AudiCars()
+        {
+            Console.WriteLine("Avaliable Audi Cars:");
+            Console.WriteLine("1 - RS6");
+            Console.WriteLine("2 - RS7");
+            Console.WriteLine("3 - A8");
+        }
+
+        public void MercedesCars()
+        {
+            
+        }
+
+        public void BmwCars()
+        {
+            
+        }
+
+        public void PorcheCars()
+        {
+            
+        }
+
+    }
+}
---
+++ CarGallery/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("\t\t\t\tWELCOME TO THE CAR SHOWROOM via Sezginler Otomotiv");
+            Console.WriteLine("\nPlease Select Your Proccess");
+            Console.WriteLine("\n1 - Buy a car");
+            Console.WriteLine("2 - Search a car");
+
+            Console.WriteLine("Please Press 1 or 2");
+            int SelectedNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (SelectedNumber == 1)
+            {
+                Console.WriteLine("Whic Brand Do You Want to Buy?");
+                string EnteredCarNames = Console.ReadLine();
+
+                Car car1 = new Car();
+
+                if (EnteredCarNames == "Audi")
+                {
+                    car1.Cars(EnteredCarNames);
+
+                    Console.WriteLine("Please Select Your Car Model");
+                    int SelectedCarModel = Convert.ToInt32(Console.ReadLine());
+                }
+
+                else if (EnteredCarNames == "Mercedes")
+                {
+                    
+                }
+
+                else if (EnteredCarNames == "BMW")
+                {
+
+                }
+
+                else if (EnteredCarNames == "Porche")
+                {
+
+                }
+            }
+
+            else
+            {
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
