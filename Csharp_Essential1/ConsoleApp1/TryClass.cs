b26fcbd9917ce49affa87c1d2df31493af7b0934
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-04-28 23:40:18 +0300

    Add a line

--- Csharp_Essential1/ConsoleApp1/TryClass.cs
+++ Csharp_Essential1/ConsoleApp1/TryClass.cs
@@ -9,6 +9,7 @@ namespace LearningCsharp
     public class TryClass
     {
         // make properties
+        // this is an example
         public int MyProperty1 { get; set; }
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
